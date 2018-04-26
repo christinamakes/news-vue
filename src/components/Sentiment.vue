982e46693ef599e16d9d463d141b6c9ec4e0d974
Christina Moore <sayhitochristina@gmail.com>
2018-04-26 12:58:28 -0500

    sentiment and overall score working

--- src/components/Sentiment.vue
+++ src/components/Sentiment.vue
@@ -3,13 +3,14 @@
     <v-layout row wrap justify-center>
         <v-flex xs12 s12 md4 lg4>
           <v-card dark color="red">
-            <v-card-title>Overall sentiment score</v-card-title>
+            <v-card-title>Overall score</v-card-title>
             <v-card-text>{{ overallScore }}</v-card-text>
           </v-card>
         </v-flex>
         <v-flex xs12 s12 md4 lg4>
-          <v-card dark color="secondary">
-            <v-card-text>{{ overallSentiment }}</v-card-text>
+          <v-card>
+            <v-card-text>The sentiment score of the most positive article was: <span class='green-txt'>{{ mostPositive }}</span></v-card-text>
+            <v-card-text>The sentiment score of the most negative article was: <span class='red-txt'>{{ mostNegative }}</span></v-card-text>
           </v-card>
         </v-flex>
         <v-flex xs12 s12 md4 lg4>
@@ -30,7 +31,21 @@ export default {
     overallScore: Number,
     mood: String,
     moodColor: String,
-    search: String
+    search: String,
+    mostPositive: Number,
+    mostNegative: Number
   }
 };
 </script>
+
+<style scoped>
+.green-txt {
+  font-weight: bold;
+  color: #4caf50;
+}
+
+.red-txt {
+  font-weight: bold;
+  color: #f44336;
+}
+</style>
